c626ad772029be552bb6919dee8106a096272c60
Miguel Ángel Ramírez <migrmrz@gmail.com>
2024-04-20 02:03:13 -0600

    Refactoring plugin structure

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,59 @@
+return {
+  'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
+  dependencies = {
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+
+    -- LSP Support
+    'neovim/nvim-lspconfig',
+
+    -- Autocompletion
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/nvim-cmp',
+    'L3MON4D3/LuaSnip',
+  },
+  config = function()
+    local lsp_zero = require('lsp-zero').preset({})
+
+    lsp_zero.on_attach(function(client, bufnr)
+      -- see :help lsp-zero-keybindings
+      -- to learn the available actions
+      lsp_zero.default_keymaps({ buffer = bufnr })
+      local opts = { buffer = bufnr, remap = false }
+
+      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+      vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+      vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+      vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+      vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+      vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+      vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+      vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+      vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    end)
+
+    require('mason').setup()
+    require('mason-lspconfig').setup({
+      -- Replace the language servers listed here 
+      -- with the ones you want to install
+      ensure_installed = {
+        'golangci_lint_ls',
+        'gopls',
+        'html',
+        'jsonls',
+        'biome',
+        'tsserver',
+        'jedi_language_server',
+        'pyre',
+        'pyright',
+        'hydra_lsp',
+        'yamlls',
+        'lua_ls',
+      },
+      handlers = {
+        lsp_zero.default_setup,
+      },
+    })
+  end
+}
