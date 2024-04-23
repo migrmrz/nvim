817d245dc98e647f3bd3d764e01aa8b47b9a9a06
Miguel Ángel Ramírez <migrmrz@gmail.com>
2024-04-22 22:52:20 -0600

    Updated mapping for neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,6 +8,6 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    vim.keymap.set('n', '<C-t>', ':Neotree<CR>')
+    vim.keymap.set('n', '<leader>et', '<cmd>Neotree<CR>')
   end,
 }
