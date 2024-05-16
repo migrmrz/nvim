a19dd0488484ff3dc313ccf8cc7a58d7a55a36b0
Miguel Ángel Ramírez <migrmrz@gmail.com>
2024-05-16 01:20:49 -0600

    Adding gitsigns as dependency for fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,7 +1,14 @@
-return
-{
+return {
   'tpope/vim-fugitive',
+
+  dependencies = {
+    'lewis6991/gitsigns.nvim'
+  },
+
   config = function()
     vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+
+    require('gitsigns').setup()
   end
+
 }
